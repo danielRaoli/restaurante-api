2b38c35d3e41f74b0216894ba6b4bc397028ac05
VictoROliveira-Dev <victorramos123@outlook.com>
2025-03-06 17:24:53 -0300

    feat: install cors types and configure cors origin in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,34 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+
 import mesaRoutes from "./routes/mesa.route";
 import pedidoRoutes from "./routes/pedido.route";
 import produtoRoutes from "./routes/produto.route";
 import categoriaRoutes from "./routes/categoria.route";
 import subCategoriaRoutes from "./routes/subcategoria.route";
 
-const app = express();  
+import cors from "cors";
+
+const app = express();
 const prisma = new PrismaClient();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/mesas", mesaRoutes(prisma));
-app.use("/pedidos", pedidoRoutes(prisma));  
+app.use("/pedidos", pedidoRoutes(prisma));
 app.use("/produtos", produtoRoutes(prisma));
 app.use("/categorias", categoriaRoutes(prisma));
 app.use("/subcategorias", subCategoriaRoutes());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
