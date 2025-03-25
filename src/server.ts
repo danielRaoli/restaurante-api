44b832e8bb158b0ced3f00a101d70a6e110f96f4
VictoROliveira-Dev <victorramos123@outlook.com>
2025-03-25 15:58:50 -0300

    chore: adjust file server.ts

--- src/server.ts
+++ src/server.ts
@@ -49,6 +49,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
